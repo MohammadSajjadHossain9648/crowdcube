bf8bc0c3f78bb8c7c439625b69ce7ad49749fbd5
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-12-08 21:45:37 +0600

    Add loading spinner in PrivateRoute component

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,20 +1,36 @@
-import React, { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { authContext } from '../AuthProvider/AuthProvider';
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { authContext } from "../AuthProvider/AuthProvider";
+import { Audio } from "react-loader-spinner";
 
-const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(authContext);
-    const location = useLocation();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(authContext);
+  const location = useLocation();
 
-    if(loading){
-        return <h2>Loading...</h2>
-    }
+  if (loading) {
+    return (
+      <div className="flex items-center">
+        <h2 className="text-2xl text-green-700">Loading</h2>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+      </div>
+    );
+  }
 
-    if(!user){
-        return <Navigate state={{from:location.pathname}} to={'/LogIn'}></Navigate>
-    }
+  if (!user) {
+    return (
+      <Navigate state={{ from: location.pathname }} to={"/LogIn"}></Navigate>
+    );
+  }
 
-    return children;
+  return children;
 };
 
 export default PrivateRoute;
